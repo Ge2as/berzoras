c4af1306511e1df2180fef550b240e81246ee9b4
Ge2as <94806960+Ge2as@users.noreply.github.com>
2026-05-11 21:05:09 +0300

    Add files via upload

--- en.2cf6c9fe.js
+++ en.2cf6c9fe.js
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44399" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44601" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
